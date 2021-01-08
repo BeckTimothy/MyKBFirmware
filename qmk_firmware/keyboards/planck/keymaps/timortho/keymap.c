416c77db8603c10b53a35f895115af800ddf5d21
Timothy Beck <johndaveleet@gmail.com>
2021-01-08 11:04:08 -0700

    modified layers 3 and 4 in preparation of project PhpStormPro

--- qmk_firmware/keyboards/planck/keymaps/timortho/keymap.c
+++ qmk_firmware/keyboards/planck/keymaps/timortho/keymap.c
@@ -18,8 +18,8 @@ enum planck_keycodes {
   PLOVER,
   BACKLIT,
   EXT_PLV,
-  KCrevBracOpen,
-  KCrevBracClosed
+  RV_LBRC,
+  RV_RBRC
 };
 
 #define LOWER MO(_LOWER)
@@ -84,37 +84,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
+ * |      |      |      |      |      |      |      |      |  7   |  8   |  9   |  +   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
+ * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |      |  4   |  5   |  6   |  -   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | | Home | End  |      |
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |   1  |  2   |  3   |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * |      |      |      |      |      |             |      |   0  |  .   |   /  |  *   |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
-    _______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_P7, KC_P8,   KC_P9,   KC_PPLS,
+    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, KC_P4, KC_P5,   KC_P6,   KC_PMNS,
+    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_P1, KC_P2,   KC_P3,   _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_P0, KC_PDOT, KC_PSLS, KC_PAST
 ),
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * | F12  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Del  |  `   |  '  |   <   |  (   |  {[  |  }]  |   )  |   >  | -_   |  =+  |  \   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |Pg Up |Pg Dn |      |
+ * |      |      |      |      |      |      |      |  End | Home |Pg Dn |Pg Up |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    KC_DEL,  KC_GRV,   KC_QUOT,   KC_LABK,   KC_LPRN,   KCrevBracOpen,   KCrevBracClosed,   KC_RPRN, KC_RABK,  KC_MINS, KC_EQL, KC_BSLS,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
+    KC_F12,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+    KC_DEL,  KC_GRV,   KC_QUOT,   KC_LABK,   KC_LPRN,   RV_LBRC,   RV_RBRC,   KC_RPRN, KC_RABK,  KC_MINS, KC_EQL, KC_BSLS,
+    _______, _______, _______, _______, _______, _______, _______,  KC_END, KC_HOME, KC_PGDN, KC_PGUP, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
@@ -232,30 +232,28 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-    case KCrevBracOpen:
-
+    case RV_LBRC:
     	  if (record->event.pressed) {
-    	    if (get_mods() & MOD_MASK_SHIFT) { // act as square bracket if shift is pressed
+    	    if (get_mods() & MOD_MASK_SHIFT) { // act as left square bracket if shift is pressed
     	        del_mods(MOD_MASK_SHIFT);
     	        register_code(KC_LBRACKET);
     	        unregister_code(KC_LBRACKET);
     	        register_code(KC_LSFT);
-             } else {
+             } else { // otherwise act as left curly brace
                register_code16(KC_LEFT_CURLY_BRACE);
                unregister_code16(KC_LEFT_CURLY_BRACE);
              }
          }
          return false;
          break;
-    	case KCrevBracClosed:
-
+    	case RV_RBRC:
     		if (record->event.pressed) {
-    		  if (get_mods() & MOD_MASK_SHIFT) { // act as square bracket if shift is pressed
+    		  if (get_mods() & MOD_MASK_SHIFT) { // act as right square bracket if shift is pressed
     		  	      del_mods(MOD_MASK_SHIFT);
             	  	register_code(KC_RBRACKET);
             	  	unregister_code(KC_RBRACKET);
             	  	register_code(KC_LSFT);
-                 } else {
+                 } else { // otherwise act as right curly brace
                  	register_code16(KC_RIGHT_CURLY_BRACE);
                  	unregister_code16(KC_RIGHT_CURLY_BRACE);
                  }
